b4874166fc79378b21c553722c967e5512f8c6dd
ayataro kogo <litailang@gmail.com>
2012-03-05 01:18:44 +0900

    add jsonic sample

--- src/main/java/com/karatebancho/servlet/SuggestServlet.java
+++ src/main/java/com/karatebancho/servlet/SuggestServlet.java
@@ -1,11 +1,15 @@
 package com.karatebancho.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.arnx.jsonic.JSON;
 
 public class SuggestServlet extends HttpServlet {
 
@@ -14,7 +18,10 @@ public class SuggestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getWriter().write("<html>Hello World.</html>");
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("readings", "hoge");
+		map.put("basicform", "basic");
+		resp.getWriter().write("<html>" + JSON.encode(map) + "</html>");
 	}
 
 }
